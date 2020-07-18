fe812fb22a09b3b89c273b3e62d17a21b32dbbea
Janaina Marana <jmarana@Janainas-MacBook-Pro.local>
2020-07-18 14:57:06 +0200

    updating css

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import '../App.css';
+import {ReactComponent as Swipe} from '../images/hand-point-up-solid.svg';
+import {ReactComponent as ArrowRight} from '../images/arrow-right-solid.svg';
+import {ReactComponent as ArrowLeft} from '../images/arrow-left-solid.svg';
 
 function Instructions(props) {
   const {showInstructions, keyboardInstructions, swipeInstructions} = props;
   // console.log('event status: ',props);
   return (
     <div className="instructions">
-      <p className={swipeInstructions ? "touch-show" : "touch-hide"}>Swipe for more</p>
-      <p className={showInstructions ? "hover-show" : "hover-hide"}>Scroll for more</p>
+      <p className={swipeInstructions ? "touch-show" : "touch-hide"}>
+        Swipe for more
+        <span><Swipe /></span>
+      </p>
+      <p className={showInstructions ? "hover-show" : "hover-hide"}>
+        <span><ArrowLeft /></span>
+        Scroll for more
+        <span><ArrowRight /></span>
+      </p>
       <p className={keyboardInstructions ? "focus-show" : "focus-hide"}>Use your arrow keys for more</p>
     </div>
   );
